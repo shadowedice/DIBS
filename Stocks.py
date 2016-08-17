47b205643f9122ac5a3f2d69e2bba084b31b2dd7
Alex <karpowich22@gmail.com>
2016-08-17 01:08:39 +0000

    Fixed stock data scrape after Yahoo! went and completely changed their web pages, those tenacious nerds.

--- Stocks.py
+++ Stocks.py
@@ -6,40 +6,40 @@ from bs4 import BeautifulSoup
 class Stocks:
     def __init__(self, bot):
         self.bot = bot
-    
-    def card_image(self):
-        link = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=%s&type=card" % self.cardId
-        imgname = self.cardId + ".jpg"
-        ur.urlretrieve(link, imgname)
-        return imgname
         
     @commands.command()
     async def stock(self, name : str):
         """Grabs the stock graph of your choice"""
         #Insurance against typical Greg shenanigans
         if name.lower() == 'greg':
-            return 'Greg is currently valued at $0.00. How sad.'
+            await self.bot.say('Greg is currently valued at $0.00. How sad.')
+            return
     
-        url = 'http://finance.yahoo.com/q?s={}'.format(name)
+        url = 'https://finance.yahoo.com/quote/%s?p=%s' % (name, name)
         stock = ur.urlopen(url).read()
         soup = BeautifulSoup(stock, 'html.parser')
-        nameData = soup.find('div', class_="title")
-        if nameData is None:
+        nameData = " "
+        priceData = " "
+
+        for h1 in soup.findAll('h1', class_="D(ib) Fz(18px)"):
+            nameData = h1.get_text()
+            
+        for span in soup.findAll('span', class_="Fw(500) D(ib) Fz(36px)"):
+            priceData = span.get_text()
+
+        # If the stock results are not found, Yahoo! redirects to a search
+        if "Search Results" in nameData:
             await self.bot.say('There is no market data for that ticker value.')
-        else:
-            companyName = nameData.find('h2').getText()
-            priceData = soup.find('span', class_="time_rtq_ticker")
-            currentPrice = priceData.getText()
+            
+        #Return a string with desired data
+        await self.bot.say('Grabbing the stock data for: {}! May the market be ever in your favor!\n\n{}:\nCurrently valued at: ${}'.format(name.upper(), nameData, priceData))
         
-            #Return a string with desired data
-            await self.bot.say('Grabbing the stock data for: {}! May the market be ever in your favor!\n{}:\nCurrently valued at: ${}'.format(name.upper(), companyName, currentPrice))
-        
-        imageData = soup.find('div', class_="chart")
-        if imageData is None:
-            await self.bot.say('There is no graph data for that ticker value.')
-        else:
-            chartURL = imageData.find('img')['src']
-            imgname = "chart.jpg"
-            ur.urlretrieve(chartURL, imgname)
-            await self.bot.upload(imgname)
-            os.remove(imgname)
+        # imageData = soup.find('div', class_="chart")
+        # if imageData is None:
+        #     await self.bot.say('There is no graph data for that ticker value.')
+        # else:
+        #     chartURL = imageData.find('img')['src']
+        #     imgname = "chart.jpg"
+        #     ur.urlretrieve(chartURL, imgname)
+        #     await self.bot.upload(imgname)
+        #     os.remove(imgname)
