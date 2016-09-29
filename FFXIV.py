e2af668cc8cc5e437af6c402ff1854afb11175c0
Brandon <shadowedice1011@gmail.com>
2016-09-29 03:16:54 +0000

    Added Pam command

--- FFXIV.py
+++ FFXIV.py
@@ -7,21 +7,29 @@ class FFXIV:
 
     @commands.command(pass_context=True)
     async def xanthe(self, ctx):
-        ret = "Xanthe and his crazy puns are now at: %d" % self.getPunCount()
+        ret = "Xanthe and his crazy puns are now at: %d" % self.getFileCount("punCount.txt")
         await self.bot.say(ret)
         try:
             await SoundEffect.playEffect(self.bot, ctx.message.author.voice_channel, 'Level Down.mp3')
         except AttributeError:
-            pass
-        
-    def getPunCount(self):
+            print("Error: Failed in xanthe callback")
+    @commands.command(pass_context=True)
+    async def pam(self, ctx):
+        ret = "Pam has meditated %d times now!" % self.getFileCount("meditationCount.txt")
+        await self.bot.say(ret)
         try:
-            file = open("punCount.txt", "r+")
+            await SoundEffect.playEffect(self.bot, ctx.message.author.voice_channel, 'Pam Sound.mp3')
+        except AttributeError:
+            print("Error: Failed in pam callback")
+        
+    def getFileCount(self, filename):
+        try:
+            file = open(filename, "r+")
             value = int(file.readline())
             value += 1
             file.seek(0)
         except FileNotFoundError:
-            file = open("punCount.txt", "a+")
+            file = open(filename, "a+")
             value = 1
         file.write(str(value))
         file.close()
