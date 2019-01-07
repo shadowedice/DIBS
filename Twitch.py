414c4d55a1c728cf6c57876df10d7c689b7aab64
Brandon <shadowedice1011@gmail.com>
2019-01-07 21:04:16 +0000

    Changed Twitch to post when people are live, delete the message when they go offline. Also now says which game they are currently playing and will update.

--- Twitch.py
+++ Twitch.py
@@ -10,7 +10,9 @@ class Twitch:
         self.database = database
         self.apiID = apiID
         self.session = None
+        self.requests = 0
         self.onlineUsers = []
+        self.gamesDict = {}
         
         
 
@@ -18,29 +20,37 @@ class Twitch:
         self.session = aiohttp.ClientSession(headers={'Client-ID' : self.apiID})
         while True:
             #For each twitch user check their status
-            req = 0
+            self.requests = 0
             for user in self.database.GetFields("Users", ["Twitch"], "notNull", ["ServerId", "UserID", "Twitch"]):
-                async with self.session.get('https://api.twitch.tv/helix/streams?user_login=' + user[2]) as resp:
-                    req += 1
-                    json = await resp.json()
-                    if json['data']:
-                        if user not in self.onlineUsers:
-                            self.onlineUsers.append(user)
-                            channel = self.database.GetField("BotChannels", ["ServerId", "Type"], [user[0], "Twitch"], ["ChannelID"])
-                            if channel:
-                                server = self.bot.get_server(user[0])
-                                await self.bot.send_message(server.get_channel(channel[0]), "{} went live on Twitch! You can watch their stream here:\n https://www.twitch.tv/{}".format(server.get_member(user[1]).name, user[2]))
-                    else:
-                        if user in self.onlineUsers:
-                            channel = self.database.GetField("BotChannels", ["ServerId", "Type"], [user[0], "Twitch"], ["ChannelID"])
-                            if channel:
-                                server = self.bot.get_server(user[0])
-                                await self.bot.send_message(server.get_channel(channel[0]), "{} has ended their stream.".format(server.get_member(user[1]).name))
-                            self.onlineUsers.remove(user)
-                if req > TWITCH_LIMIT:
+                channel = self.database.GetField("BotChannels", ["ServerId", "Type"], [user[0], "Twitch"], ["ChannelID"])
+                if channel:
+                    async with self.session.get('https://api.twitch.tv/helix/streams?user_login=' + user[2]) as resp:
+                        self.requests += 1
+                        json = await resp.json()
+                        curUser = [x for x in self.onlineUsers if x[0] == user[0] and x[1] == user[1] and x[2] == user[2]]
+                        server = self.bot.get_server(user[0])
+                        #If live
+                        if json['data']:
+                            gameID = json['data'][0]['game_id']
+                            msgText = "{} is live playing **{}** on Twitch! You can watch their stream here:\n https://www.twitch.tv/{}".format(server.get_member(user[1]).name, await self.__getGameName(gameID), user[2])
+                            #if not in the current user list
+                            if not curUser:
+                                message = await self.bot.send_message(server.get_channel(channel[0]), msgText)
+                                self.onlineUsers.append(user + (message.id,))
+                            #Is in list, check for updates
+                            else:
+                                await self.bot.edit_message(server.get_channel(channel[0]), msgText)
+                        #Not live
+                        else:
+                            #Was in live list
+                            if curUser:
+                                await self.bot.delete_message(await self.bot.get_message(server.get_channel(channel[0]), curUser[0][3]))
+                                self.onlineUsers.remove(curUser[0])
+                                
+                if self.requests > TWITCH_LIMIT:
                     await asynico.sleep(60)
-                    req = 0
-            await asyncio.sleep(30)
+                    self.requests = 0
+            await asyncio.sleep(120)
             
     @commands.command(pass_context=True)
     async def addTwitch(self, ctx, name : str):
@@ -63,3 +73,15 @@ class Twitch:
     async def closeSession(self):
         self.session.close()
         self.session = None
+        
+    async def __getGameName(self, gameID):
+        if gameID not in self.gamesDict:
+            async with self.session.get('https://api.twitch.tv/helix/games?id=' + gameID) as resp:
+                self.requests += 1
+                json = await resp.json()
+                if json['data']:
+                    name = json['data'][0]['name']
+                    self.gamesDict[gameID] = name
+                    return name
+        else:
+            return self.gamesDict[gameID]
