367521a6a389b36fbedb3cea34057c992ae36a9d
Brandon <shadowedice1011@gmail.com>
2019-02-22 19:46:07 +0000

    Added %change to stocks report

--- Stocks.py
+++ Stocks.py
@@ -18,6 +18,11 @@ class Stocks:
             async with aiohttp.request('GET', 'https://api.iextrading.com/1.0/stock/' + name + '/quote') as resp:
                 try:
                     tickerInfo = json.loads(await resp.text())
-                    await self.bot.say("{} ({})\nLast Updated: {}\nValue: ${}".format(tickerInfo['companyName'], name.upper(), tickerInfo['latestTime'], tickerInfo['latestPrice']))
+                    change = str(tickerInfo['changePercent'] * 100)
+                    #add plus sign for positive value
+                    if tickerInfo['changePercent'] > 0:
+                        change = "+" + change
+                        
+                    await self.bot.say("{} ({})\nLast Updated: {}\nValue: ${}\nChange: {}%".format(tickerInfo['companyName'], name.upper(), tickerInfo['latestTime'], tickerInfo['latestPrice'], change))
                 except:
                     await self.bot.say("I'm sorry but I can't find {} ticker!".format(name.upper()))
